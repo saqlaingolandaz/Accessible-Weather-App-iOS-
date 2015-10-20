f19c157310a59732d75c3b85b1cd2f1c0693a929
saqlaingolandaz <saqlaingolandaz@hotmail.com>
2015-10-19 22:43:38 -0400

    weekly weather json setup

--- Accessible App/WeeklyWeatherClass.swift
+++ Accessible App/WeeklyWeatherClass.swift
@@ -9,14 +9,34 @@
 import UIKit
 
 class WeeklyWeatherClass: UIViewController {
+    
+    
+    @IBOutlet weak var time1Label: UILabel!
+    
+    var time1String:String = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        weeklyData()
     }
     
     func weeklyData() {
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string:"http://api.wunderground.com/api/16337742f9b11efe/forecast10day/q/autoip.json")
+        let returnedData = NSData(contentsOfURL: url!)
+        var error: NSError?
+        var parsedData: NSDictionary = NSJSONSerialization.JSONObjectWithData(returnedData!, options: NSJSONReadingOptions.MutableContainers, error: &error)as NSDictionary
+        
+        var main:NSDictionary = parsedData["forecast"] as NSDictionary
+        var simpleForecast:NSDictionary = main["simpleforecast"] as NSDictionary
+        var forecastDay:NSArray = simpleForecast["forecastday"] as NSArray
+        var days:NSDictionary = forecastDay[0] as NSDictionary
+        var date1:NSDictionary = days["date"] as NSDictionary
+        self.time1String = date1["weekday"] as NSString
+        self.time1Label.text = self.time1String
+        
+
         
     }
 
